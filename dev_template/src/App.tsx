1b1c9e9e715a169342cbea91c5400e3c55b66126
Adalton Junio <adalton.jteixeira@mercadolivre.com>
2023-01-13 19:58:28 -0300

    Adding dev_template and render the react elements

---
+++ dev_template/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Payment from '../../src/bricks/payment'
+
+export default function App() {
+  return (
+    <div className="App">
+        <Payment />
+    </div>
+  );
+}
---
+++ src/bricks/payment/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Payment = () => {
+    return <div id='paymentBrick_container'>Render bricks here!</div>
+}
+
+export default Payment
