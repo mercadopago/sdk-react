3058674177e40fe6e1acafbc1a541ef059471837
Leticia Mazzo <leticia.mazzo@mercadolivre.com>
2023-01-30 17:04:56 -0300

    document Payment Brick render function

--- src/bricks/payment/index.tsx
+++ src/bricks/payment/index.tsx
@@ -3,6 +3,33 @@ import { onErrorDefault, onReadyDefault, onSubmitDefault } from '../util/initial
 import { initBrick } from '../util/renderBrick';
 import { PaymentType } from './type';
 
+/**
+ * Payment Brick allows you to add several payment methods to your store with just one Brick, allowing you to save card data for future purchases.
+ *
+ * Usage:
+ *
+ * ```ts
+ * import Payment, {useMercadoPago} from '@mercadopago/sdk-react'
+ *
+ * useMercadoPago('YOUR_PUBLIC_KEY')
+ *
+ * const Example = () => {
+ *   return(
+ *    <Payment
+        config={
+          initialization: {amount: 100}, customization: {paymentMethods: {}}
+        }
+        onSubmit={async () => {}}
+        onError={() => {}}
+        onReady={() => {}}
+      />
+ *   )
+ * }
+ * export default Example
+ * ```
+ *
+ * @tutorial {@link https://www.mercadopago.com.br/developers/en/docs/checkout-bricks/payment-brick/introduction Payment Brick documentation} for more information.
+ */
 const BrickPayment = ({
   config,
   onSubmit = onSubmitDefault,
