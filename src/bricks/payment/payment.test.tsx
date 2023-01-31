b36a18c14bab3296f6db27e9cbdce06b868650b4
francineemilia <francine.costa@mercadolivre.com>
2023-01-31 14:08:58 -0300

    fix: correct payment tests change

--- src/bricks/payment/payment.test.tsx
+++ src/bricks/payment/payment.test.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import CardPayment from './index';
+import Payment from './index';
 
-const config = {};
+const config = {
+  initialization: {
+    amount: 100,
+  },
+  customization: {
+    paymentMethods: {
+      bankTransfer: ['pix'],
+      ticket: 'all',
+      atm: 'all',
+      creditCard: 'all',
+      debitCard: 'all',
+      mercadoPago: 'all',
+    },
+  },
+};
 
 test('renders the Payment Brick', () => {
   render(
-    <CardPayment
+    <Payment
       config={config}
       onSubmit={async (param) => {
         console.log(param);
