5f20fb8efbc2c8ee00bf6566cc5d1cb3d0c76f0b
Leticia Mazzo <leticia.mazzo@mercadolivre.com>
2023-02-06 17:23:29 -0300

    add documentation customization payments types

--- src/bricks/payment/type.ts
+++ src/bricks/payment/type.ts
@@ -1,4 +1,8 @@
-import { ICardPaymentBrickPayer, ICardPaymentBrickVisual, ICardPaymentFormData } from '../cardPayment/type';
+import {
+  ICardPaymentBrickPayer,
+  ICardPaymentBrickVisual,
+  ICardPaymentFormData,
+} from '../cardPayment/type';
 import {
   IBrickStyle,
   IBrickCustomVariables,
@@ -16,9 +20,9 @@ export type InstanceMercadoPagoType = {
 };
 
 export type TPaymentType = {
-  onSubmit: (param: IPaymentFormData, param2?: IAdditionalData) => Promise<unknown>;  //todo: deveria ser void
+  onSubmit: (param: IPaymentFormData, param2?: IAdditionalData) => Promise<unknown>;
   onReady?: () => void;
-  onError?: (param: IBrickError) => void; //todo: deveria ser unknown
+  onError?: (param: IBrickError) => void;
   onBinChange?: (param: string) => void;
   /**
    * An object containing initialization options.
@@ -71,10 +75,14 @@ interface IPaymentFormData {
    *
    * @see {@link https://www.mercadopago.com/developers/en/docs/checkout-bricks/payment-brick/payment-submission/other-payment-methods/brasil Returned data} documentation.
    */
-  formData: //todo: não entendo o que isso diz // rever
-    & ICardPaymentFormData<ICardPaymentBrickPayer> 
-    & ICardPaymentFormData<ISavedCardPayer> 
-    & TicketFormData;
+  formData: ICardPaymentFormData<ICardPaymentBrickPayer> &
+    ICardPaymentFormData<ISavedCardPayer> &
+    TicketFormData;
+  /**
+   * Bin of the card entered by the user.
+   *
+   * @see {@link https://www.mercadopago.com/developers/en/docs/checkout-bricks/payment-brick/additional-customization/additional-data additional data customization} documentation.
+   */
   additionalData?: IAdditionalData;
 }
 
@@ -84,36 +92,126 @@ interface ISavedCardPayer {
 }
 
 interface TicketFormData {
+  /**
+   * Ticket transaction amount.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+   */
   transaction_amount: number;
+  /**
+   * Ticket payment method id.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+   */
   payment_method_id: string;
+  /**
+   * Ticket payer data.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+   */
   payer: IPayerAPI;
 }
 
 interface IPaymentBrickCustomization {
+  /**
+   * Control the visual aspects of the brick.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+   */
   visual?: TPaymentBrickVisual;
+  /**
+   * Configuration of wich payment methods will be accepted
+   *
+   * @see {@link https://www.mercadopago.com.br/developers/en/docs/checkout-bricks/payment-brick/configure-integration/other-payment-methods/brasil Customizable data} documentation.
+   */
   paymentMethods: TPaymentBrickPaymentMethods;
 }
 
+/**
+ * Control the visual aspects of the brick.
+ *
+ * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+ */
 type TPaymentBrickVisual = IPaymentBrickBaseVisual & IPaymentBrickVisual;
 
+/**
+ * Control the visual aspects of the brick.
+ *
+ * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+ */
 interface IPaymentBrickBaseVisual
   extends IBrickVisual<IPaymentBrickCustomizableTexts, IPaymentBrickStyle>,
     ICardPaymentBrickVisual {}
 
 interface IPaymentBrickVisual {
+  /**
+   * Hide redirection panel.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+   */
   hideRedirectionPanel?: boolean;
+  /**
+   * Preserve saved cards order.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+   */
   preserveSavedCardsOrder?: boolean;
+  /**
+   * Preserve saved cards order.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+   */
   defaultPaymentOption?: {
+    /**
+     * Form loads with credit card selected
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+     */
     creditCardForm?: boolean;
+    /**
+     * Form loads with debit card selected
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+     */
     debitCardForm?: boolean;
+    /**
+     * Form loads with saved card
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+     */
     savedCardForm?: string;
+    /**
+     * Form loads with ticket selected 
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+     */
     ticketForm?: boolean;
+    /**
+     * Form loads bank transfer selected
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+     */
     bankTransferForm?: boolean;
+    /**
+     * Form loads with Mercado Pago Wallet selected.
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+     */
     walletForm?: boolean;
+    /**
+     * Form loads with Mercado Pago Credits selected.
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md additional data customization} documentation.
+     */
     creditForm?: boolean;
   };
 }
 
+/**
+ * Configuration of wich payment methods will be accepted
+ *
+ * @see {@link https://www.mercadopago.com.br/developers/en/docs/checkout-bricks/payment-brick/configure-integration/other-payment-methods/brasil Customizable data} documentation.
+ */
 type TPaymentBrickPaymentMethods = IPaymentBrickPaymentMethods &
   (
     | { creditCard: AllOrArray }
@@ -127,34 +225,114 @@ type TPaymentBrickPaymentMethods = IPaymentBrickPaymentMethods &
 type AllOrArray = 'all' | string[];
 
 interface IPaymentBrickPaymentMethods {
+  /**
+   * Maximum number of customizable installments
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   maxInstallments?: number;
+  /**
+   * Minimum number of customizable installments
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   minInstallments?: number;
+  /**
+   * Define which payment methods will be accepted or not.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   types?: {
+    /**
+     * Define which payment methods will not be accepted.
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+     */
     excluded?: TPaymentBrickPaymentType[];
+    /**
+     * Define which payment methods will be accepted.
+     *
+     * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+     */
     included?: TPaymentBrickPaymentType[];
   };
 }
 
 interface ILabelPlaceholder {
-  label?: string
-  placeholder?: string
+  /**
+   * Custom texts
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
+  label?: string;
+  placeholder?: string;
 }
 
 interface IPaymentBrickCustomizableTexts {
+  /**
+   * Custom payer first name
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md  Customizable data} documentation.
+   */
   payerFirstName?: ILabelPlaceholder;
+  /**
+   * Custom payer last name
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   payerLastName?: ILabelPlaceholder;
+  /**
+   * Custom payer zip code
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   zipCode?: ILabelPlaceholder;
+  /**
+   * Custom payer state
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   addressState?: ILabelPlaceholder;
+  /**
+   * Custom address city
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   addressCity?: ILabelPlaceholder;
+  /**
+   * Custom address neighborhood
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   addressNeighborhood?: ILabelPlaceholder;
+  /**
+   * Custom address street
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   addressStreet?: ILabelPlaceholder;
+  /**
+   * Custom address number
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   addressNumber?: { label?: string };
+  /**
+   * Custom address complement
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Customizable data} documentation.
+   */
   addressComplement?: { label?: string };
 }
 
 interface IPaymentBrickStyle extends IBrickStyle<IPaymentBrickCustomVariables> {}
 
 interface IPaymentBrickCustomVariables extends IBrickCustomVariables {
+  /**
+   * Custom variable
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/bricks/payment.md Consult Payer data} documentation.
+   */
   secondaryColor?: string;
 }
 
@@ -231,11 +409,19 @@ interface IAddress {
   /**
    * Payer complement that can start already filled in.
    *
-   * @see {@link https://www.mercadopago.com/developers/en/docs/checkout-bricks/payment-brick/additional-customization/initialize-data-on-the-bricks Data customization} documentation.
+   * @see {@link https://www.mercadopago.com.br/developers/en/docs/checkout-bricks/payment-brick/additional-customization/initialize-data-on-the-bricks Data customization} documentation.
    */
   complement?: string;
 }
 
+/**
+ * Payment types.
+ *
+ * @see {@link https://www.mercadopago.com.br/developers/en/docs/checkout-bricks/payment-brick/configure-integration/cards Cards type documentation}
+ * @see {@link https://www.mercadopago.com.br/developers/en/docs/checkout-bricks/payment-brick/configure-integration/wallet-credits Mercado Pago Wallet and Installments without card documentation},
+ * @see {@link https://www.mercadopago.com.br/developers/en/docs/checkout-bricks/payment-brick/configure-integration/pix Pix documentation}
+ * @see {@link https://www.mercadopago.com.br/developers/en/docs/checkout-bricks/payment-brick/configure-integration/other-payment-methods/brasil Other payment methods documentation}.
+ */
 type TPaymentBrickPaymentType =
   | 'atm'
   | 'ticket'
