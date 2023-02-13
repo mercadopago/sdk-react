f167ef287240a419c7a0cbcb80e9c2293f95d0a0
Adalton Junio <adalton.jteixeira@mercadolivre.com>
2023-02-13 19:08:41 -0300

    Fix npm folders

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import WalletBrick from './bricks/wallet';
-import PaymentBrick from './bricks/payment';
-import CardPaymentBrick from './bricks/cardPayment';
-
-export { WalletBrick, PaymentBrick, CardPaymentBrick };
