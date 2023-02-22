5ec71cb6be2b7d3a92f7b7fc83be0b3e1fe812a4
Adalton Junio <adalton.jteixeira@mercadolivre.com>
2023-02-22 09:44:20 -0300

    Adding index file on root folder

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import initMercadoPago from './mercadoPago/initMercadoPago';
+
+import CardPayment from './bricks/cardPayment';
+import Payment from './bricks/payment';
+import StatusScreen from './bricks/statusScreen';
+import Wallet from './bricks/wallet';
+
+export { initMercadoPago, CardPayment, Payment, StatusScreen, Wallet };
