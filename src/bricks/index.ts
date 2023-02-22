dddce3280437961396e55a34d3ea58bac830b3cf
Adalton Junio <adalton.jteixeira@mercadolivre.com>
2023-02-22 10:14:47 -0300

    Adding index file on bricks folder

---
+++ src/bricks/index.ts
@@ -0,0 +1,6 @@
+import CardPayment from './cardPayment';
+import Payment from './payment';
+import StatusScreen from './statusScreen';
+import Wallet from './wallet';
+
+export { CardPayment, Payment, StatusScreen, Wallet };
