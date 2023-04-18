0a2417c0ff447fcb36b9a7c2a458d733a2a6b15a
AleFossati <alexandre.fossati@mercadolivre.com>
2023-04-18 16:01:02 -0300

    added jsdoc to getIdentificationTypes method

--- src/coreMethods/getIdentificationTypes/index.ts
+++ src/coreMethods/getIdentificationTypes/index.ts
@@ -1,5 +1,10 @@
 import { MercadoPagoInstance } from "../../mercadoPago/initMercadoPago";
 
+/**
+ * Return all the document types based on the public_key
+ * 
+ * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/core-methods.md#mp-instancegetidentificationtypes Core Methods - getIdentificationTypes} documentation.
+ */
 const getIdentificationTypes = async () => {
   const instanceMercadoPago = await MercadoPagoInstance.init();
   return instanceMercadoPago?.getIdentificationTypes();
