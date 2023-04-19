05542cf6ce894e24495d2194d2232cfb355c053d
AleFossati <alexandre.fossati@mercadolivre.com>
2023-04-19 16:33:40 -0300

    removing getIdentificationType properties documentation

--- src/coreMethods/getIdentificationTypes/index.ts
+++ src/coreMethods/getIdentificationTypes/index.ts
@@ -3,7 +3,8 @@ import { MercadoPagoInstance } from "../../mercadoPago/initMercadoPago";
 /**
  * Return all the document types based on the public_key
  * 
- * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/core-methods.md#mp-instancegetidentificationtypes Core Methods - getIdentificationTypes} documentation.
+ * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/core-methods.md#mp-instancegetidentificationtypes method documentation}.
+ * @see {@link https://www.mercadopago.com/developers/en/reference/identification_types/_identification_types/get response documentation}.
  */
 const getIdentificationTypes = async () => {
   const instanceMercadoPago = await MercadoPagoInstance.init();
--- src/coreMethods/getIdentificationTypes/types.ts
+++ src/coreMethods/getIdentificationTypes/types.ts
@@ -1,36 +1,7 @@
 export type IdentificationType = {
-  /**
-   * Identification type id.
-   * 
-   * @see {@link https://www.mercadopago.com/developers/en/reference/identification_types/_identification_types/get API Reference - identification_types}.
-   */
   id: string,
-
-  /**
-   * Identification type name.
-   * 
-   * @see {@link https://www.mercadopago.com/developers/en/reference/identification_types/_identification_types/get API Reference - identification_types}.
-   */
   name: string,
-
-  /**
-   * Identification number data type.
-   * 
-   * @see {@link https://www.mercadopago.com/developers/en/reference/identification_types/_identification_types/get API Reference - identification_types}.
-   */
   type: string,
-
-  /**
-   * Identification number min length.
-   * 
-   * @see {@link https://www.mercadopago.com/developers/en/reference/identification_types/_identification_types/get API Reference - identification_types}.
-   */
   min_length: number,
-
-  /**
-   * Identification number max length.
-   * 
-   * @see {@link https://www.mercadopago.com/developers/en/reference/identification_types/_identification_types/get API Reference - identification_types}.
-   */
   max_length: number
 }
