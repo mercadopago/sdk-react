5dcb85e0b57d80de4eeaf0c769cc1a89dd1e30d0
AleFossati <alexandre.fossati@mercadolivre.com>
2023-04-19 16:43:15 -0300

    removed @description tag

--- src/coreMethods/getPaymentMethods/index.ts
+++ src/coreMethods/getPaymentMethods/index.ts
@@ -2,7 +2,7 @@ import { MercadoPagoInstance } from "../../mercadoPago/initMercadoPago";
 import type { TPaymentMethodsParams } from "./types";
 
 /**
- * @description Returns a payment methods list.
+ * Returns a payment methods list.
  * 
  * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/core-methods.md#mp-instancegetpaymentmethodspaymentmethodsparams method documentation}.
  * @see {@link https://www.mercadopago.com/developers/en/reference/payment_methods/_payment_methods/get response documentation}.
