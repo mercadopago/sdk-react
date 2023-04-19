f3643249b1c61eee74d134a44aa1ff29562ecdaf
AleFossati <alexandre.fossati@mercadolivre.com>
2023-04-19 16:31:28 -0300

    added jsdoc for getPaymentMethods

--- src/coreMethods/getPaymentMethods/index.ts
+++ src/coreMethods/getPaymentMethods/index.ts
@@ -1,6 +1,12 @@
 import { MercadoPagoInstance } from "../../mercadoPago/initMercadoPago";
 import type { TPaymentMethodsParams } from "./types";
 
+/**
+ * @description Returns a payment methods list.
+ * 
+ * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/core-methods.md#mp-instancegetpaymentmethodspaymentmethodsparams method documentation}.
+ * @see {@link https://www.mercadopago.com/developers/en/reference/payment_methods/_payment_methods/get response documentation}.
+ */
 const getPaymentMethods = async (paymentMethodsParams: TPaymentMethodsParams) => {
   const instanceMercadoPago = await MercadoPagoInstance.init();
   return instanceMercadoPago?.getPaymentMethods(paymentMethodsParams);
