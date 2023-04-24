47658a7043f621d672fe34ac1e461055f29a93e0
francineemilia <francine.costa@mercadolivre.com>
2023-04-24 12:18:20 -0300

    fix: adds loaded instance check and handle changes of PK and options

--- examples/bricks/CardPayment/0-Simple.tsx
+++ examples/bricks/CardPayment/0-Simple.tsx
@@ -4,6 +4,10 @@ import Card from '../../../src/bricks/cardPayment';
 import initMercadoPago from '../../../src/mercadoPago/initMercadoPago';
 
 initMercadoPago('TEST-f4563544-ce69-40c3-b88e-6e7d1bd93a83', { locale: 'pt-BR' });
+setTimeout(
+  () => initMercadoPago('APP_USR-72435728-2951-4991-984f-d137a027ed9c', { locale: 'pt-BR' }),
+  1000,
+);
 
 const App = () => {
   return (
