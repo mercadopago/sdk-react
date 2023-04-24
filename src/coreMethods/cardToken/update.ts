4b01f8020f8a2b6cb3fa3cb4608efbb013688efb
AleFossati <alexandre.fossati@mercadolivre.com>
2023-04-24 16:03:56 -0300

    added jsdoc to updateCardToken method

--- src/coreMethods/cardToken/update.ts
+++ src/coreMethods/cardToken/update.ts
@@ -1,6 +1,7 @@
 import { MercadoPagoInstance } from "../../mercadoPago/initMercadoPago";
 import type { TCardTokenUpdateParams } from "./types";
 
+/** Token update method. */
 const updateCardToken = async (paymentMethodsParams: TCardTokenUpdateParams) => {
   const instanceMercadoPago = await MercadoPagoInstance.getInstance();
   return instanceMercadoPago?.updateCardToken(paymentMethodsParams);
