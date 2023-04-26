4a70d1243bdc73345a9ffac09af29216d40d2e6f
AleFossati <alexandre.fossati@mercadolivre.com>
2023-04-26 17:05:48 -0300

    added jsdoc for some properties and more specific properties for style type

--- src/secureFields/util/types.ts
+++ src/secureFields/util/types.ts
@@ -1,19 +1,74 @@
 export type IFieldStyle = {
-  [key: string]: string;
+  color?: string;
+  "font-family"?: string;
+  fontFamily?: string;
+  "font-size"?: string;
+  fontSize?: string;
+  "font-style"?: string;
+  fontStyle?: string;
+  "font-variant"?: string;
+  fontVariant?: string;
+  "font-weight"?: string;
+  fontWeight?: string;
+  height?: string;
+  margin?: string;
+  "margin-bottom"?: string;
+  marginBottom?: string;
+  "margin-left"?: string;
+  marginLeft?: string;
+  "margin-right"?: string;
+  marginRight?: string;
+  "margin-top"?: string;
+  marginTop?: string;
+  padding?: string;
+  "padding-bottom"?: string;
+  paddingBottom?: string;
+  "padding-left"?: string;
+  paddingLeft?: string;
+  "padding-right"?: string;
+  paddingRight?: string;
+  "padding-top"?: string;
+  paddingTop?: string;
+  "placeholder-color"?: string;
+  placeholderColor?: string;
+  "text-align"?: string;
+  textAlign?: string;
+  width?: string;
 };
 
 export type ICustomFonts = {
+  /** URL from where to load the custom font */
   src: string;
 };
 
 export type TBaseFieldsOptions = {
+  /**
+   * Defines input placeholder.	
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/fields.md#params-2 Placeholder}.
+   */
   placeholder?: string;
+  /**
+   * Defines field styles with keys named as CSS property
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/fields.md#style Style}.
+   */
   style?: IFieldStyle;
+  /**
+   * Custom Fonts is an array with src attribute defining an url from where to load a custom font.
+   *
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/fields.md#custom-fonts Custom Fonts}.
+   */
   customFonts?: ICustomFonts[];
   group?: string;
 };
 
 export interface IDateYearFieldsOptions extends TBaseFieldsOptions {
+  /** 
+   * For 'short' year must have two digits, for 'full' year must have four digits.
+   * 
+   * @see {@link https://github.com/mercadopago/sdk-js/blob/main/API/fields.md#year-mode Year mode}.
+   * */
   mode?: 'short' | 'full'
 }
 
