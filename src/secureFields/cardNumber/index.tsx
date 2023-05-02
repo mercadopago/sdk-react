99bc73c7487c5b41c57a6cea5752e97df152d0a0
AleFossati <alexandre.fossati@mercadolivre.com>
2023-05-02 09:16:35 -0300

    fix cardNumberInstance = undefined

--- src/secureFields/cardNumber/index.tsx
+++ src/secureFields/cardNumber/index.tsx
@@ -2,15 +2,25 @@ import React, { useEffect } from 'react';
 import type { TCardNumberParams } from './types';
 import type { IField } from '../util/types';
 import { initSecureField } from '../util';
+import { DEBOUNCE_TIME_RENDER } from '../../bricks/util/constants';
 
 let cardNumberInstance: IField | undefined = undefined;
 
 const CardNumber = (params: TCardNumberParams) => {
 
   useEffect(() => {
-    initSecureField('cardNumber', params).then(instance => cardNumberInstance = instance);
+    // CardPayment uses a debounce to prevent unnecessary reRenders.
+    let timer: ReturnType<typeof setTimeout>;
 
-    return () => cardNumberInstance?.unmount();
+    timer = setTimeout(() => {
+      initSecureField('cardNumber', params).then(instance => cardNumberInstance = instance);
+    }, DEBOUNCE_TIME_RENDER);
+
+    return () => {
+      clearTimeout(timer);
+      cardNumberInstance?.unmount();
+      cardNumberInstance = undefined;
+    }
   }, []);
 
   return <div id="cardNumberSecureField_container"></div>;
