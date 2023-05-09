b6341b7c72eff803e7276cfa85bb02e4572d5622
AleFossati <alexandre.fossati@mercadolivre.com>
2023-05-09 08:46:27 -0300

    added exports

--- src/secureFields/index.ts
+++ src/secureFields/index.ts
@@ -1,3 +1,7 @@
-import cardNumber from "./cardNumber";
+import CardNumber from "./cardNumber";
+import SecurityCode from "./securityCode";
+import ExpirationDate from "./expirationDate";
+import ExpirationMonth from "./expirationMonth";
+import ExpirationYear from "./expirationYear";
 
-export { cardNumber };
+export { CardNumber, SecurityCode, ExpirationDate, ExpirationMonth, ExpirationYear };
