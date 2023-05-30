7b7a5b6f25584dbc0ffb5a5ab6ff03f188aa1207
francineemilia <francine.costa@mercadolivre.com>
2023-05-30 14:11:51 -0300

    chore: add 3DS challenge to storybook

---
+++ examples/bricks/StatusScreen/3-3DS-Challenge.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import StatusScreen from '../../../src/bricks/statusScreen';
+
+import initMercadoPago from '../../../src/mercadoPago/initMercadoPago';
+
+initMercadoPago('TEST-2b00e5a0-b421-4588-b9cb-846a553d760e', { locale: 'pt-BR' });
+
+const App = () => {
+  const initialization = {
+    paymentId: '1307907697',
+    additionalInfo: {
+      externalResourceURL: 'https://eoql5ybmp0gwf41.m.pipedream.net',
+      creq: 'teste',
+    },
+  };
+
+  return <StatusScreen initialization={initialization} onError={(error) => console.error(error)} />;
+};
+
+export default App;
