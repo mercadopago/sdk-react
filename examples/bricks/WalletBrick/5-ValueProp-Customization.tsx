a9c9d5e2d5916312b955235d2b4c66e6aeb4706e
Ismael Caldana <ismael.caldana@mercadolivre.com>
2023-06-12 13:01:02 -0300

    Update examples/bricks/WalletBrick/5-ValueProp-Customization.tsx
    Co-authored-by: André <andre.agostinho@mercadolivre.com>

--- examples/bricks/WalletBrick/5-ValueProp-Customization.tsx
+++ examples/bricks/WalletBrick/5-ValueProp-Customization.tsx
@@ -5,7 +5,7 @@ import initMercadoPago from '../../../src/mercadoPago/initMercadoPago';
 
 initMercadoPago('TEST-f4563544-ce69-40c3-b88e-6e7d1bd93a83');
 
-const ExampleVisualCustomizationWalletBrick = () => {
+const ExampleValuePropCustomizationWalletBrick = () => {
   return (
     <Wallet
       initialization={{ preferenceId: 'YOUR_PREFERENCE_ID' }}
