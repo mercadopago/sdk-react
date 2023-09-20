43423ef4486f480a09c0b72fc01015f52de2174b
Leticia Mazzo <leticia.mazzo@mercadolivre.com>
2023-09-20 12:38:48 -0300

    start review and confirm adding a payment integration

---
+++ examples/bricks/Payment/3-Review-Confirm.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Payment from '../../../src/bricks/payment';
+
+import initMercadoPago from '../../../src/mercadoPago/initMercadoPago';
+
+initMercadoPago('TEST-f4563544-ce69-40c3-b88e-6e7d1bd93a83', { locale: 'pt-BR' });
+
+const App = () => {
+  const initialization = {
+    amount: 100,
+    preferenceId: '207446753-ea3adb2e-a4f2-41dd-a656-11cb01b8772c',
+  };
+
+  const customization = {
+    paymentMethods: {
+      atm: 'all',
+      ticket: 'all',
+      bankTransfer: ['pix'],
+      creditCard: 'all',
+      debitCard: 'all',
+      mercadoPago: 'all',
+    },
+  };
+
+  return (
+    <Payment
+      initialization={initialization}
+      customization={customization}
+      onSubmit={async (param) => {
+        console.log(param);
+      }}
+    />
+  );
+};
+
+export default App;
