aaa105d0397cd2249d6c5312ce74e9cc1006261b
Leticia Mazzo <leticia.mazzo@mercadolivre.com>
2023-09-22 12:43:07 -0300

    adjusts on simple example

--- examples/bricks/Payment/4-Simple-Review-Confirm.tsx
+++ examples/bricks/Payment/4-Simple-Review-Confirm.tsx
@@ -8,7 +8,7 @@ initMercadoPago('TEST-d198443d-7e9f-4e5f-a770-e5b23ae627cb', { locale: 'es-MX' }
 const App = () => {
   const initialization = {
     amount: 1000,
-    preferenceId: '',
+    preferenceId: '1283129650-ea525882-af60-4895-b954-e8814a6c5b3c',
     items: {
       totalItemsAmount: 331.14,
       itemsList: [
@@ -24,7 +24,6 @@ const App = () => {
       firstName: 'Ana',
       lastName: 'Silva',
     },
-    // todo: remove discounts after merge CHOBK-1020 and update beta envinroment
     discounts: {
       totalDiscountsAmount: 10,
       discountsList: [
