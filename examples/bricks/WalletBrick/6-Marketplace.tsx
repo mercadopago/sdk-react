77b4c7d45436036217afe8788b55e4d21dcc56ef
Adalton Júnio <adalton.jteixeira@mercadolivre.com>
2023-10-03 14:01:59 -0300

    Update examples/bricks/WalletBrick/6-Marketplace.tsx
    Co-authored-by: André <andre.agostinho@mercadolivre.com>

--- examples/bricks/WalletBrick/6-Marketplace.tsx
+++ examples/bricks/WalletBrick/6-Marketplace.tsx
@@ -6,7 +6,7 @@ import initMercadoPago from '../../../src/mercadoPago/initMercadoPago';
 initMercadoPago('TEST-f4563544-ce69-40c3-b88e-6e7d1bd93a83');
 
 const ExampleWalletBrickMarketplace = () => {
-  return <Wallet initialization={{ preferenceId: 'YOUR_PREFERENCE_ID', marketplace: true }} />;
+  return <Wallet initialization={{ preferenceId: '207446753-ea3adb2e-a4f2-41dd-a656-11cb01b8772c', marketplace: true }} />;
 };
 
 export default ExampleWalletBrickMarketplace;
