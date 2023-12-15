2d4d7ba4224a5dc4b4f92064732e6512a0ca0e3c
Leticia Mazzo <leticia.mazzo@mercadolivre.com>
2023-12-15 16:03:48 -0300

    add unit tests

---
+++ src/bricks/brand/brand.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MercadoPagoInstance } from '../../mercadoPago/initMercadoPago';
+import Brand from './index';
+
+describe('Test Brand Brick Component', () => {
+  test('should found the id of Brand Brick div', async () => {
+    MercadoPagoInstance.publicKey = 'PUBLIC_KEY';
+    const element = await render(<Brand />);
+
+    expect(element.container.querySelector('#brandBrick_container')).toBeTruthy();
+  });
+
+  test('should found the id of Brand Brick div', async () => {
+    MercadoPagoInstance.publicKey = 'PUBLIC_KEY';
+    const element = await render(
+      <Brand
+        customization={{
+          text: {
+            valueProp: 'payment_methods_logos',
+          },
+        }}
+        onReady={() => console.log('Brick is ready!')}
+      />,
+    );
+
+    expect(element.container.querySelector('#brandBrick_container')).toBeTruthy();
+  });
+});
