677bc9cccc5450a49b1af28a46a2c3dc06c71a52
rafaeloledo <rafaeloliveiraledo@gmail.com>
2025-04-16 00:45:17 -0300

    fix: add index to coreMethods to resolve imports
    I've tried to import like suggested in the README as
    
    `import { createCardToken } from '@mercadopago/sdk-react/esm/coreMethods';`
    
    without the changes, i need to import like this
    
    `import createCardToken from '@mercadopago/sdk-react/esm/coreMethods/cardToken/create';`

---
+++ src/coreMethods/index.ts
@@ -0,0 +1,7 @@
+import createCardToken from "./cardToken/create";
+import updateCardToken from "./cardToken/update";
+import getIdentificationTypes from "./getIdentificationTypes";
+import getInstallments from "./getInstallments";
+import getIssuers from "./getIssuers";
+import getPaymentMethods from "./getPaymentMethods";
+export { createCardToken, updateCardToken, getIdentificationTypes, getInstallments, getIssuers, getPaymentMethods };
